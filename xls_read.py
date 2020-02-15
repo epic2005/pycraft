6907650a03b76f9f258ce333caf7ad61993a29cc
李 朋越 <lipengyue@gmail.com>
2020-02-15 10:03:47 +0800

    #update20200215

--- xls_read.py
+++ xls_read.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 # read from api
 
 from pyExcelerator import *
-sheets = parse_xls('mini.xls')
+sheets = parse_xls('./xls/vms.xls')
 
-print sheets
+print sheets[0][1][(0, 0)]
+print sheets[0][1][(0, 1)]
+print sheets[0][1][(0, 2)]
+
+print sheets[0][1][(1, 0)]
+print sheets[0][1][(2, 0)]
+print sheets[0][1][(3, 0)]
